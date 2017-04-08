bd42d2c7e58fd1a9d273600f6b37c135adc41731
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-08 13:17:01 +0530

    Add action to 'new'controller

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+   actions: {
+    addTask: function () {
+      var title = this.get("title");
+      var desc = this.get("description");
+      var date = this.get("date");
+
+      // Create a  new task with all fields
+
+
+    }
+  }
 });
