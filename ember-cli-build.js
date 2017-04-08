e27953ff6dea2a212657d848797fafd4caa0af5c
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-08 16:14:49 +0530

    Added formdate helper

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,6 +26,6 @@ module.exports = function(defaults) {
   // app.import("bower_components/bootstrap/dist/css/bootstrap.css")
   // app.import("bower_components/bootstrap/dist/js/bootstrap.js")
 
-  app.import("bower_components/moment/min/moment.min.js")
+  app.import("bower_components/moment/moment.js");
   return app.toTree();
 };
