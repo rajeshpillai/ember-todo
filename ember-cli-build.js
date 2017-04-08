898b6f14412f5e5ce76ef049688c941ef6e7767e
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-08 15:50:37 +0530

    Installed moment.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,5 +26,6 @@ module.exports = function(defaults) {
   // app.import("bower_components/bootstrap/dist/css/bootstrap.css")
   // app.import("bower_components/bootstrap/dist/js/bootstrap.js")
 
+  app.import("bower_components/moment/min/moment.min.js")
   return app.toTree();
 };
